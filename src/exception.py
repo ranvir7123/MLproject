eeab94acdabe15517f8d6c7e30bdc1e51429a7ca
Ranvir Makker <ranvir7123@gmail.com>
2025-07-12 21:36:45 +0530

    params

--- src/exception.py
+++ src/exception.py
@@ -1,18 +1,19 @@
+# src/exception.py
 import sys
-def error_message_detail(error,error_detail:sys):
-    _,_,exc_tb=error_detail.exc_info()
-    file_name=exc_tb.tb_frame.f_code.co_filename
-    error_message = 'erroe accured in python script name[{0}],line number[{1}],error message[{2}]'.format(
-    file_name,exc_tb.tb_lineno,str(error))
 
+def error_message_detail(error, error_detail: sys):
+    _, _, exc_tb = error_detail.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    error_message = (
+        f"Error occurred in Python script [{file_name}], "
+        f"line [{exc_tb.tb_lineno}]: {str(error)}"
+    )
     return error_message
-    
-     
 
 class CustomException(Exception):
-    def __init__(self,error_message,error_detail:sys):
+    def __init__(self, error_message, error_detail: sys):
         super().__init__(error_message)
-        self.error_message = error_message_detail(error_message,error_detail=error_detail)
+        self.error_message = error_message_detail(error_message, error_detail)
 
     def __str__(self):
         return self.error_message
